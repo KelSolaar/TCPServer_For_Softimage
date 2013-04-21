a46447bf2e7a5068b2879ed3b21821e735288630
Thomas Mansencal <thomas.mansencal@gmail.com>
2013-04-22 00:22:12 +0200

    Update system encoding manipulation.

---
+++ utilities/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**__init__.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+
+**Others:**
+
+"""
+
+#**********************************************************************************************************************
+#***	Module attributes.
+#**********************************************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2013 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+__all__ = ["DEFAULT_CODEC"]
+
+DEFAULT_CODEC = "utf-8"
+
+#**********************************************************************************************************************
+#***	Encoding manipulations.
+#**********************************************************************************************************************
+def _setEncoding():
+	"""
+	This definition sets the Application encoding.
+	"""
+
+	import sys
+	reload(sys)
+
+	sys.setdefaultencoding(DEFAULT_CODEC)
+
+_setEncoding()
+
+
